b4f1dd79d197679bb8b0e648ed69b562431b428c
joeplaydon <141444027+joeplaydon@users.noreply.github.com>
2025-02-24 13:52:08 +0000

    - Add quickstart examples

---
+++ quickstart_import.py
@@ -0,0 +1,42 @@
+import weaviate
+from weaviate.classes.init import Auth
+import requests, json, os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Best practice: store your credentials in environment variables
+wcd_url = os.environ["WCD_URL"]
+wcd_api_key = os.environ["WCD_API_KEY"]
+cohere_api_key = os.environ["COHERE_API_KEY"]
+
+client = weaviate.connect_to_weaviate_cloud(
+    cluster_url=wcd_url,                                    # Replace with your Weaviate Cloud URL
+    auth_credentials=Auth.api_key(wcd_api_key),             # Replace with your Weaviate Cloud key
+    headers={"X-Cohere-Api-Key": cohere_api_key},           # Replace with your Cohere API key
+)
+
+resp = requests.get(
+    "https://raw.githubusercontent.com/weaviate-tutorials/quickstart/main/data/jeopardy_tiny.json"
+)
+data = json.loads(resp.text)
+
+questions = client.collections.get("Question")
+
+with questions.batch.dynamic() as batch:
+    for d in data:
+        batch.add_object({
+            "answer": d["Answer"],
+            "question": d["Question"],
+            "category": d["Category"],
+        })
+        if batch.number_errors > 10:
+            print("Batch import stopped due to excessive errors.")
+            break
+
+failed_objects = questions.batch.failed_objects
+if failed_objects:
+    print(f"Number of failed imports: {len(failed_objects)}")
+    print(f"First failed object: {failed_objects[0]}")
+
+client.close()  # Free up resources
